6819af5de10e2913804c6c7cf413c602dc177b75
root <tehran88>
2024-12-13 13:30:17 +0000

    C - Binary Trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,37 +1,19 @@
 #include "binary_trees.h"
+
 /**
- *binary_tree_balance - measures the balance factor of a binary tree
- *@tree: the root node of the tree
- *Return: balance measure
+ * binary_tree_nodes - function that counts the leaves in a binary tree
+ * @tree: pointer node binary tree
+ * Return: Number the leaves in a binary tree
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t nodes = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	return (left - right);
-}
-/**
- *binary_tree_height - measures the height of binary tree
- *@tree: the root of tree
- *Return: size of left or right
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left = 0, right = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left += binary_tree_height(tree->left);
-	right += binary_tree_height(tree->right);
-	if (left > right)
-		return (left + 1);
-	else
-		return (right + 1);
+	if (tree->left || tree->right)
+		nodes += 1;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
 }
