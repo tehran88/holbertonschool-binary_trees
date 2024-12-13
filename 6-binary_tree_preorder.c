cebbe9f69ef63c6fa138bf5ce5e537a7cd502b48
root <tehran88>
2024-12-13 12:42:39 +0000

    C - Binary Trees

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function
+ * @tree: pointer
+ * @func: function pointer
+ * Return: Nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
+
